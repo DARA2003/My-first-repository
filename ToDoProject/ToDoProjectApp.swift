8bd92bb068203485c240d4d018e27a06de37781d
Mai Huy Vu <huyvu06092003@gmail.com>
2025-12-05 13:35:45 +0700

    Initial Commit

---
+++ ToDoProject/ToDoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoProjectApp.swift
+//  ToDoProject
+//
+//  Created by Mai huy vu on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
